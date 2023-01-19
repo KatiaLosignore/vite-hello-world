887f0a91b59efe8676455906abcca8fc7d609fb6
KatiaLosignore <katialos.kl@gmail.com>
2023-01-19 16:52:23 +0100

    added compnent

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+ export default {
+  data(){
+    return{
+      name: 'Katia'
+    }
+  },
+  components: {
+    AppTitle
+  }
+    
+ }
+</script>
+
+
+<template>
+  <app-title></app-title>
+</template>
+
+
+<style>
+p {
+  color: red;
+}
+</style>
